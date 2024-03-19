dc260078ac2c2e9b67cafcc806bf6d4ac54251b9
Sayem Quazi <midmalequazi@gmail.com>
2024-03-19 02:37:46 -0400

    fix registration

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -9,6 +9,7 @@ export default function Register() {
   const [email, setEmail] = createSignal("");
   const [password, setPassword] = createSignal("");
   const [repeatPassword, setRepeatPassword] = createSignal("");
+  const [success, setSuccess] = createSignal(false);
   const [error, setError] = createSignal(undefined);
 
   const transport = new GrpcWebFetchTransport({
@@ -39,9 +40,20 @@ export default function Register() {
       password: password(),
       email: email()
     };
-    const res = await usersClient.register(registerRequest);
-    const message = res.response?.message;
-    console.log(message)
+    console.log(registerRequest);
+    try {
+      const res = await usersClient.register(registerRequest);
+      const message = res.response?.message;
+      console.log(message);
+      setEmail("");
+      setPassword("");
+      setRepeatPassword("");
+      setError(undefined);
+      setSuccess(true);
+    } catch (error) {
+      setError(error);
+      setSuccess(false);
+    }
   }
 
   return (
@@ -52,23 +64,31 @@ export default function Register() {
             <label class="label">
                 <span class="label-text text-primary">Email</span>
             </label>
-            <input name="email" type="email" placeholder="Email" class="input w-full max-w-xs" onInput={(e) => setEmail(e.target.value)}/>
+            <input name="email" type="email" placeholder="Email" class="input w-full max-w-xs" onInput={(e) => setEmail(e.target.value)} value={email()}/>
         </div>
         <div class="mb-2">
             <label class="label">
                 <span class="label-text text-primary">Password</span>
             </label>
-            <input name="password1" type="password" placeholder="Password" class="input w-full max-w-xs" onInput={(e) => setPassword(e.target.value)}/>
+            <input name="password1" type="password" placeholder="Password" class="input w-full max-w-xs" onInput={(e) => setPassword(e.target.value)} value={password()}/>
         </div>
         <div class="mb-4">
             <label class="label">
                 <span class="label-text text-primary">Repeat Password</span>
             </label>
-            <input name="password2" type="password" placeholder="Password" class="input w-full max-w-xs" onInput={(e) => setRepeatPassword(e.target.value)}/>
+            <input name="password2" type="password" placeholder="Password" class="input w-full max-w-xs" onInput={(e) => setRepeatPassword(e.target.value)} value={repeatPassword()}/>
         </div>
         <div>
           <button onClick={handleRegister} class="btn">Register</button>
         </div>
+
+        <Show when={success()}>
+          <div class="toast">
+            <div class="alert alert-info">
+              <span>User registration successful</span>
+            </div>
+          </div>
+        </Show>
     </div>
   );
 }
